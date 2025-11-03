4ef630721ed9add3e85f6d90cb5687d41fd48234
Computer8543 <college1b7e@gmail.com>
2025-11-03 10:08:24 -0500

    feat(mad-libs): implement simple mad-libs game.

---
+++ mad-libs.py
@@ -0,0 +1,6 @@
+print("Welcome to the Mad Libs game!")
+noun = input("Enter a noun: ")
+verb = input("Enter a verb: ")
+adjective = input("Enter an adjective: ")
+story = f"Once upon a time, there was a {adjective} {noun} who loved to {verb}."
+print(story)
